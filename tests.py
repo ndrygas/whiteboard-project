f27ff0baaf4666e60e2d73167e113ee0ed667e41
Nicolas Drygas <nicolas.drygas@live.com>
2023-06-12 13:24:19 -0700

    completed notes page styling

--- tests.py
+++ tests.py
@@ -53,69 +53,7 @@ class FlaskTestsDatabase(TestCase):
 
         result = self.client.get("/nico", follow_redirects=True)
         self.assertIn(b"You must be logged in to view your whiteboard", result.data)
-
-    #test notes and shared notes are in user page
-    # def test_notes_and_shared_notes(self):
-    #     """Test if user notes and shared notes are on page."""
-
-    #     session["username"] = "nico"
-    #     result = self.client.get("/nico", follow_redirects=True)
-    #     self.assertIn(b"Title-1", result.data)
-    #     self.assertIn(b"Title-2", result.data)
-
         
-
-    # def test_departments_list(self):
-    #     """Test departments page."""
-
-    #     result = self.client.get("/departments")
-    #     self.assertIn(b"Legal", result.data)
-
-    # def test_departments_details(self):
-    #     """Test departments page."""
-
-    #     result = self.client.get("/department/fin")
-    #     self.assertIn(b"Phone: 555-1000", result.data)
-
-
-# class FlaskTestsLoggedIn(TestCase):
-#     """Flask tests with user logged in to session."""
-
-#     def setUp(self):
-#         """Stuff to do before every test."""
-
-#         app.config['TESTING'] = True
-#         app.config['SECRET_KEY'] = 'key'
-#         self.client = app.test_client()
-
-#         # Start each test with a user ID stored in the session.
-#         with self.client as c:
-#             with c.session_transaction() as sess:
-#                 sess['user_id'] = 1
-
-#     def test_important_page(self):
-#         """Test important page."""
-
-#         result = self.client.get("/important")
-#         self.assertIn(b"You are a valued user", result.data)
-
-
-# class FlaskTestsLoggedOut(TestCase):
-#     """Flask tests with no logged in user in session."""
-
-#     def setUp(self):
-#         """Stuff to do before every test."""
-
-#         app.config['TESTING'] = True
-#         self.client = app.test_client()
-
-#     def test_important_page(self):
-#         """Test that user can't see important page when logged out."""
-
-#         result = self.client.get("/important", follow_redirects=True)
-#         self.assertNotIn(b"You are a valued user", result.data)
-#         self.assertIn(b"You must be logged in", result.data)
-
 
 if __name__ == "__main__":
     import unittest
